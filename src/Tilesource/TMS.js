df30c3c053ce32807fde0ceaf5098c6c24efdb65
Rainer Simon <rainer.simon@ait.ac.at>
2011-04-30 22:56:17 +0200

    TMS fixes

--- src/Tilesource/TMS.js
+++ src/Tilesource/TMS.js
@@ -2,10 +2,27 @@
  * A tilesource implementation for TMS (as supported by OpenLayers). 
  */
 No5.Seajax.Tilesource.TMS = function(baseurl, width, height) {
+   // Compute number of zoomlevels in tileset
+   var max;
+   if (width > height) {
+      max = Math.ceil(width / 256);
+   } else {
+      max = Math.ceil(height / 256);
+   }
+   var levels = Math.ceil(Math.log(max)/Math.log(2));
+
+   // Number of y tiles at highest zoom level
+   var h = Math.ceil(height / 256);
+
    var ts = new Seadragon.TileSource(width, height, 256, 0);
-   ts.getTileUrl = function(zoom, xTile, yTile) {
-      var h = Math.pow(2, zoom - 10) - 1;
-      return baseurl + (zoom - 8) + "/" + xTile + "/" +  (h - yTile) + ".jpg";
+   ts.getTileUrl = function(zoom, x, y) {
+      // Convert from Deep Zoom definition to TMS definition
+      var z = zoom - 8;
+      
+      // Number of y tiles at this zoom level
+      var yTiles = Math.ceil(h / Math.pow(2, levels - z)) - 1;
+
+      return baseurl + z + "/" + x + "/" +  (yTiles - y) + ".jpg";
    }
    return ts;
 }
